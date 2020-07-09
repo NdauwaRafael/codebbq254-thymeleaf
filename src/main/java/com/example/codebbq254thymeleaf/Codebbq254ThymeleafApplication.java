5106c05e9e192a96a04da1d9cb75f593394f1fd5
Raphael <raphaelndauwa@gmail.com>
2020-07-09 17:41:58 +0300

    initialize files'

---
+++ src/main/java/com/example/codebbq254thymeleaf/Codebbq254ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebbq254thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codebbq254ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Codebbq254ThymeleafApplication.class, args);
+    }
+
+}
